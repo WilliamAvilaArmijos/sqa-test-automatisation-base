6e47380a71f6ac29dd42922ee1055ca248563786
WILLIAM EDUARDO AVILA ARMIJOS <wavilaar@pichincha.com>
2025-06-20 12:38:32 -0500

    Updated last version

--- src/test/java/com/pichincha/TestRunner.java
+++ src/test/java/com/pichincha/TestRunner.java
@@ -9,8 +9,11 @@ class TestRunner {
 
     @Test
     void testParallel() {
+        //Para realizar las validaciones de cada una de las feature cambiar en el campo tag el
+        // identificador de cada uno de las feature en el ejemplo se esta realizando el
+        //de obtener todos los heroes 
         Results results = Runner.path("classpath:com/pichincha/features/marvel_api")
-                .tags("@character_update")
+                .tags("@character_retrieval")
                 .parallel(5);
         assertEquals(0, results.getFailCount(), results.getErrorMessages());
     }
